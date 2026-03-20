df6f166a0a6366e496ee78744bc6222587b22fb2
MydrieDybiStudios <abeme5254@gmail.com>
2026-03-20 11:03:31 +0500

    Update AdminOlympiadForm.tsx

--- src/pages/AdminOlympiadForm.tsx
+++ src/pages/AdminOlympiadForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { supabase } from '../../lib/supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 import { useNavigate, useParams } from 'react-router-dom';
 import { ArrowLeft, Save } from 'lucide-react';
 
